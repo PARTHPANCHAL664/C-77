1ed0908504c00754845d40f2f11e33876e56b35f
PARTHPANCHAL664 <78475055+PARTHPANCHAL664@users.noreply.github.com>
2023-03-10 14:47:42 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+var first_name = "Ranbir";
+var last_name = "Kapoor";
+
+console.log(first_name.concat(last_name));
+
+var a = 10;
+var b = 5;
+
+var c = 8;
+var d = 7;
+
+var subtract = c - d;
+console.log(subtract);
+
+var e = 2;
+var f = 5;
+
+var divide = e / f;
+console.log(divide);
+
+var add = a + b ;
+console.log(add);
